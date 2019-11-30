c4af6b7c89550bd3c7b3782e5ae67bcaae7f3bf9
Sergey Rudov <rudovsl.miet@gmail.com>
2019-11-30 20:23:08 +0300

    git-repository creation

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная страница";
+$arDirProperties = array(
+   "description" => "Мы предлагаем широкий ассортимент качественных часов",
+   "keywords" => "часы, время",
+   "robots" => "index, follow"
+);
+?>
